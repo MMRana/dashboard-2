d43a949aec7ef204a909754e93cf3f1d81eb99b0
MMRana <69115514+MMRana@users.noreply.github.com>
2022-09-27 19:36:04 +0600

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function() {
+    $('.brithday').click(function (){
+        $('.s-btn').toggle()
+    })
+
+    $('.info').click(function (){
+        $('.info-form').toggle()
+    })
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
